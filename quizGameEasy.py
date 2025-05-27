974b5e947a3d53f90ad112b6f9fcd4b414c145e6
Swastik Adhikari <78269644+sskadk@users.noreply.github.com>
2025-05-27 12:13:42 +0545

    quiz game easy

---
+++ quizGameEasy.py
@@ -0,0 +1,40 @@
+print("Welcome to my PC_Quiz!")
+
+playing = input("Do you want to play? (Y/N) ")
+
+if playing.lower() != "y":
+    quit()
+
+print("Okay! Let's play :)")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 4) * 100) + "%.")
